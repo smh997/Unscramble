9beab282ac926ba6bcc066a6e16c163fb0b2175b
smh10 <smh10.mh@gmail.com>
2024-11-19 19:21:20 -0500

    Another success path test is added.

--- app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -25,6 +25,21 @@ class GameViewModelTest {
     }
 
     @Test
+    fun gameViewModel_WordSkipped_ScoreUnchangedAndWordCountIncreased() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        val lastWordCount = currentGameUiState.currentWordCount
+        viewModel.skipWord()
+        currentGameUiState = viewModel.uiState.value
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+        assertEquals(lastWordCount + 1, currentGameUiState.currentWordCount)
+    }
+
+    @Test
     fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
         val incorrectPlayerWord = "dkhaskjdh"
         viewModel.updateUserGuess(incorrectPlayerWord)
@@ -61,7 +76,6 @@ class GameViewModelTest {
         assertEquals(MAX_NO_OF_WORDS, currentGameUiState.currentWordCount)
         assertTrue(currentGameUiState.isGameOver)
     }
-
 
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
